9089da1af24e5d0f9061f911bff7a97284fb71c0
Jakub Duchniewicz <j.duchniewicz@gmail.com>
2020-05-11 16:29:29 +0200

    Remove unnecessary ifdef.

--- Test/TestApp/src/Main.cpp
+++ Test/TestApp/src/Main.cpp
@@ -9,19 +9,8 @@ int main()
 {
 	pp::PluginsContainer container;
 
-// probably Unix approach is crossplatform
-#if defined(_WIN32)
-	char ownPth[MAX_PATH];
-	HMODULE hModule = GetModuleHandle(NULL);
-	if (hModule != NULL)
-	{
-		GetModuleFileName(hModule, ownPth, (sizeof(ownPth)));
-		container.load(std::filesystem::path(ownPth).parent_path(), false);
-	}
-#else
 	std::cout << "Current path: " << std::filesystem::current_path() << std::endl;
 	container.load(std::filesystem::current_path(), false);
-#endif
     
 	AddIntent intent{ 2, 3 };
 	std::optional<AddIntent::Result> result = container.getRouter()->processIntent(std::move(intent));
